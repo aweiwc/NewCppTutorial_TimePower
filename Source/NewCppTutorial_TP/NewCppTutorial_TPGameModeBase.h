12bc1303d9ec6c50da0e637c8bc722aa2e743c8b
aweiwc <1243676094@qq.com>
2026-04-10 16:19:49 +0800

    Basic System

---
+++ Source/NewCppTutorial_TP/NewCppTutorial_TPGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NewCppTutorial_TPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWCPPTUTORIAL_TP_API ANewCppTutorial_TPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
